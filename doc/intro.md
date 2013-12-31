31f46bb5e1052f134cd9dcc863b42241a3c49e6a
John Evans <lgastako@gmail.com>
2013-12-31 16:32:50 -0500

    It begins.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to leds-out
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/leds_out/core_test.clj
@@ -0,0 +1,7 @@
+(ns leds-out.core-test
+  (:require [clojure.test :refer :all]
+            [leds-out.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
