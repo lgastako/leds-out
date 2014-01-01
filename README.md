e38680893795b8afc62ce7cf4c60d44efdc3fb51
John Evans <lgastako@gmail.com>
2014-01-01 18:08:43 -0500

    The first cut is the deepest.

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 A reimagining of https://en.wikipedia.org/wiki/Lights_Out_(game) in Clojure
 using overtone to harness the Novatio Launchpad S as a UI.
 
+This depends on my [launchtone](http://github.com/lgastako/launchtone) library.
+Both the launchtone library and the game itself are brand new so expect bugs.
+
 ## Usage
 
    lein run
--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject leds-out "0.1.0-SNAPSHOT"
-  :description "leds-out game for Launchpad S."
+(defproject leds-out "0.1.0"
+  :description "A port of Light's Out / Light's Off to The Launchpad S."
   :url "http://github.com/lgastako/leds-out"
   :license {:name "Public Domain"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
   :main ^:skip-aot leds-out.core
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.1"]
-                 [launchtone "0.1.0-SNAPSHOT"]])
+                 [launchtone "0.1.0"]])
